6b5771410852d60a676d06ac3f125d6dbeeb0c47
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-05-17 14:35:39 +0530

    complete get mouse clicked points from user

--- io_helper.py
+++ io_helper.py
@@ -1,16 +1,62 @@
+from tokenize import Number
 import cv2 as cv
 import numpy as np
 
+
+NumberOfGraspPoints = 2 # number of grasp points per object
+global CurrentGraspPoint
+CurrentGraspPoint = 0
+
+# mouse callback function to draw the circle when the mouse is clicked
+def draw_circle(event,x,y,flags,params):
+    global CurrentGraspPoint
+    p = params[0]
+    if event == cv.EVENT_LBUTTONDOWN:
+        cv.circle(params[1],(x,y),2,(255,0,0),-1)
+        p.append((x,y))
+        CurrentGraspPoint += 1
+
+# fucntion to get the grasp locations from the user
 def get_grasp_locations(template_image):
+
     """
     get the grasping points of the object using the mouse clicked points
     """
+
     # list to keep the mouse clicked points
     grasp_locations = []
     
-    # [DEBUG]
-    print('[DEBUG] template_image.shape:', template_image.shape)
+    # initialize the variable to its default value
+    global CurrentGraspPoint
+    CurrentGraspPoint = 0
 
+    # create a window to show the template image
+    cv.namedWindow('Template Image', cv.WINDOW_NORMAL)
+
+    # make it full screen
+    cv.setWindowProperty('Template Image', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+
+    # arrange the params for the mouse callback function
+    params = [grasp_locations, template_image]
+
+    # set the mouse callback function
+    cv.setMouseCallback('Template Image', draw_circle, params)
+
+    # get mouse clicked points
+    print('Click the grasp points of the object')
+    while(1):
+        cv.imshow('Template Image', template_image)
+        if CurrentGraspPoint == NumberOfGraspPoints:            
+            # wait for 2 seconds
+            cv.waitKey(2000)
+
+            # break the loop
+            break
+
+        if cv.waitKey(20) & 0xFF == 27:            
+            # break the loop
+            break
+    
     # return the list of mouse clicked points
     return grasp_locations
 
--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from io_helper import *
 def main():
     
     # get the template image from the directory
-    template_image = cv.imread('template/allen_key.jpg', 0)
+    template_image = cv.imread('template/allen_key.jpg', cv.IMREAD_ANYCOLOR)
 
     # get the grasping location from the user as mouse clicked points
     grasp_locations = get_grasp_locations(template_image)
+
+    # print the mouse clicked points
+    print(grasp_locations)
+
+
+
 
 if __name__ == '__main__':
     main()
